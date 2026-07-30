0455bd2e742eede4015e337649fcd73376b44e52
Akshatadeshpande28 <akshata.jd03@gmail.com>
2026-07-30 06:01:45 +0000

    update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from app.stock import get_stock_data
 
 app = FastAPI(
     title="StockVision AI",
     version="1.0.0"
 )
+
 
 @app.get("/")
 def home():
@@ -11,3 +13,8 @@ def home():
         "message": "Welcome to StockVision AI 🚀",
         "status": "Backend Running"
     }
+
+
+@app.get("/stock/{symbol}")
+def stock_analysis(symbol: str, period: str = "1y"):
+    return get_stock_data(symbol, period)
