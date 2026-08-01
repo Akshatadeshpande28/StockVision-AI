544f37ea8380e3e24b891ea5e40ffe1ac33cc07d
Akshatadeshpande28 <akshata.jd03@gmail.com>
2026-08-01 10:28:40 +0000

    added new code to main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.stock import (
-    get_stock_data,
-    get_multi_period_analysis,
+
+app = FastAPI(
+    title="StockVision AI",
+    version="1.0.0"
 )
 
-app = FastAPI
-title="StockVision AI",
-version="1.0.0"
-
 app.add_middleware(
-CORSMiddleware,
-allow_origins=["*"],   # For development
-allow_credentials=True,
-allow_methods=["*"],
-allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 from app.technical import get_technical_analysis
 
