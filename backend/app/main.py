7ae3247e44447ce2425711d9e3e2b2266cb0c5f8
Akshatadeshpande28 <akshata.jd03@gmail.com>
2026-08-21 15:14:55 +0000

    update stock details in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.stock import get_stock_data, get_multi_period_analysis
+
+
 app = FastAPI(
     title="StockVision AI",
     version="1.0.0"
 )
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,3 +25,13 @@ def home():
         "message": "Welcome to StockVision AI 🚀",
         "status": "Backend Running"
     }
+
+
+@app.get("/stock/{symbol}")
+def stock_analysis(symbol: str, period: str = "1y"):
+    return get_stock_data(symbol, period)
+
+
+@app.get("/analysis/{symbol}")
+def analysis(symbol: str):
+    return get_multi_period_analysis(symbol)
